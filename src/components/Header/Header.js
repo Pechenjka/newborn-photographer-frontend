29ac4beb6316d15e3750e748d3fadb3d85431dec
Petr Lobachev <petya.lobachev@yandex.ru>
2021-09-26 22:49:33 +0300

    feat: добавить бургер меню в header и адаптировать под мобильную версию

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,15 @@
 import "./Header.scss";
 import photographer from "../../images/logo-header-photographer.png";
 import NavMenu from "../NavMenu/NavMenu";
+import { closeBurgerMenu, openBurgerMenu } from "../../redux/Actions/userAction";
+
+import { useDispatch, useSelector } from "react-redux";
 
 function Header({ timerRef }) {
+  const dispatch = useDispatch();
+  const burgerMenu = useSelector((state) => state.user.burgerMenu);
+
+  //Изменение цвета header при прокрутки страницы
   window.onscroll = () => {
     const header = document.querySelector(".header");
     if (window.pageYOffset > 70) {
@@ -12,11 +19,30 @@ function Header({ timerRef }) {
     }
   };
 
+  //Обработчик открытия закрытия бургер-меню
+  const handleOpenAndCloseBurgerMenu = () => {
+    if (burgerMenu) {
+      const link = document.querySelectorAll(".navigation__link-arrow");
+      const containerWithSubLinks = document.querySelectorAll(".navigation__subLinks-container");
+      link.forEach((item) => item.classList.remove("subLinks__active"));
+      containerWithSubLinks.forEach((item) => item.classList.remove("navigation__subLinks-container_active"));
+      dispatch(closeBurgerMenu());
+    } else {
+      dispatch(openBurgerMenu());
+    }
+  };
+
   return (
     <header className="header">
       <div className="header__container">
         <img className="header__logo" src={photographer} alt="лого фотографа" />
-        <NavMenu timerRef={timerRef} />
+        <div
+          className={`header__burger-icon ${burgerMenu ? "header__burger-icon_close" : ""}`}
+          onClick={handleOpenAndCloseBurgerMenu}
+        >
+          <span></span>
+        </div>
+        <NavMenu timerRef={timerRef} handleOpenAndCloseBurgerMenu={handleOpenAndCloseBurgerMenu} />
       </div>
     </header>
   );
--- src/components/NavMenu/NavMenu.js
+++ src/components/NavMenu/NavMenu.js
@@ -3,7 +3,8 @@ import "./NavMenu.scss";
 import { links, packets } from "../../utils/config";
 import {
   buttonOfTheTypePhotos,
-  deleteAllPhotosOneType, displayPricePackets,
+  deleteAllPhotosOneType,
+  displayPricePackets,
   handlePricePackets,
   handleTypesPhotos,
   notDisplayPhotosOnThePagePhotoGallery,
@@ -11,70 +12,104 @@ import {
 } from "../../redux/Actions/userAction";
 import { useDispatch, useSelector } from "react-redux";
 
-function NavMenu({ timerRef }) {
+function NavMenu({ timerRef, handleOpenAndCloseBurgerMenu }) {
   const dispatch = useDispatch();
+  const burgerMenu = useSelector((state) => state.user.burgerMenu);
   const displayPhotos = useSelector((state) => state.photos.displayPhotosOnThePagePhotoGallery);
 
   //Обработчик клика по ссылке подменю
   const handleClickDropdownLink = (type, path) => {
     clearInterval(timerRef.current);
     dispatch(displayPricePackets([]));
-    //dispatch(showGalleryPhotos([]));
-    //dispatch(deleteAllPhotosOneType([]));
     dispatch(buttonOfTheTypePhotos(""));
-   if (displayPhotos) {
+    if (displayPhotos) {
       dispatch(handleTypesPhotos(type, path));
     }
     dispatch(handlePricePackets(packets, path));
-
     dispatch(notDisplayPhotosOnThePagePhotoGallery());
-
+    if (window.innerWidth <= 768) {
+      handleOpenAndCloseBurgerMenu();
+    }
   };
 
-  const handleClickLink = () => {
+  //Обработчик клика по ссылке меню
+  const handleClickLink = (path) => {
     clearInterval(timerRef.current);
-    dispatch(showGalleryPhotos([]));
-    dispatch(buttonOfTheTypePhotos(""));
+    if (window.innerWidth <= 768) {
+      handleOpenAndCloseBurgerMenu();
+    }
+    if (path !== "/") {
+      dispatch(showGalleryPhotos([]));
+      dispatch(buttonOfTheTypePhotos(""));
+    }
     dispatch(deleteAllPhotosOneType([]));
     dispatch(notDisplayPhotosOnThePagePhotoGallery());
     dispatch(displayPricePackets([]));
   };
+  //Обработчик клика по ссылке меню
+  const handleClickLinkMobileVersion = (linkActive) => {
+    const link = document.querySelectorAll(".navigation__link-arrow");
+    const containerWithSubLinks = document.querySelectorAll(".navigation__subLinks-container");
+    if (window.innerWidth <= 768) {
+      link.forEach((item) => {
+        if (item.textContent === linkActive.name) {
+          item.classList.toggle("subLinks__active");
+        }
+      });
+      containerWithSubLinks.forEach((el) => {
+        if (el.childElementCount === linkActive.select.length) {
+          el.classList.toggle("navigation__subLinks-container_active");
+          el.classList.toggle("navigation__view-lists-links_active");
+        }
+      });
+    }
+  };
 
   return (
-    <nav className="navigation">
-      <ul className="navigation__list_links">
+    <nav
+      className={`navigation ${burgerMenu ? "navigation__burgerMenu_active" : ""}`}
+      onClick={handleOpenAndCloseBurgerMenu}
+    >
+      <ul className="navigation__list_links" onClick={(e) => e.stopPropagation()}>
         {links.map((item, index) => {
           return (
             <li className="navigation__container-link navigation__view-lists-links" key={index}>
               {item.select ? (
-                <div className={`navigation__link ${item.select ? "navigation__link-arrow" : ""}`}>{item.name}</div>
+                <div
+                  className={`navigation__link  ${item.select ? "navigation__link-arrow" : ""}`}
+                  onClick={() => handleClickLinkMobileVersion(item)}
+                >
+                  {item.name}
+                </div>
               ) : (
                 <NavLink
                   className={`navigation__link ${item.select ? "navigation__link-arrow" : ""}`}
                   activeClassName="navigation__link_active"
                   exact={true}
                   to={item.path}
-                  onClick={handleClickLink}
+                  onClick={() => handleClickLink(item.path)}
                 >
                   {item.name}
                 </NavLink>
               )}
-              <div className="navigation__select-container navigation__view-lists-links_active">
-                {Array.isArray(item.select) &&
-                  item.select.map((el, index) => {
-                    return (
-                      <NavLink
-                        className="navigation__select-link"
-                        activeClassName="navigation__select-link_active"
-                        to={el.pathSelect}
-                        key={index}
-                        onClick={() => handleClickDropdownLink(el.type, el.pathSelect)}
-                      >
-                        {el.name}
-                      </NavLink>
-                    );
-                  })}
-              </div>
+              {item.select && (
+                <div className={`navigation__subLinks-container navigation__view-lists-links_active`}>
+                  {Array.isArray(item.select) &&
+                    item.select.map((el, index) => {
+                      return (
+                        <NavLink
+                          className="navigation__sublink"
+                          activeClassName="navigation__sublink_active"
+                          to={el.pathSelect}
+                          key={index}
+                          onClick={() => handleClickDropdownLink(el.type, el.pathSelect)}
+                        >
+                          {el.name}
+                        </NavLink>
+                      );
+                    })}
+                </div>
+              )}
             </li>
           );
         })}
@@ -82,4 +117,5 @@ function NavMenu({ timerRef }) {
     </nav>
   );
 }
+
 export default NavMenu;

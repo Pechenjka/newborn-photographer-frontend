b90f28e441e313335df270967d2553de19fdb75a
Petr Lobachev <lobachevpetr@gmail.com>
2022-09-14 04:31:22 +0300

    feat: update navmenu

--- src/components/InfoToolTip/InfoToolTip.tsx
+++ src/components/InfoToolTip/InfoToolTip.tsx
@@ -1,17 +1,23 @@
-import "./InfoToolTip.scss";
+import Styles from "./style.module.scss";
 import React from "react";
-import { useAppSelector } from "../../redux/hooks";
+import classNames from "classnames/bind";
 
 export interface PropsInfoToolTip {
   text: string;
+  error?: boolean;
+  animation?: boolean;
 }
+const InfoToolTip: React.FC<PropsInfoToolTip> = ({ text, error = false, animation = false }) => {
 
-const InfoToolTip: React.FC<PropsInfoToolTip> = ({ text }) => {
-  const { errorSendEmail } = useAppSelector((state) => state.app);
+  const cx = classNames.bind(Styles);
+  const classNamesContainer = cx("infoToolTip", { infoToolTip_animation: animation }, { infoToolTip_error: error });
+  const classNamesText = cx("infoToolTip__text", { infoToolTip__text_error: error });
+
   return (
-    <div className={`infoToolTip ${errorSendEmail && "infoToolTip__error"}`}>
-      <p className={`infoToolTip__text ${errorSendEmail && "infoToolTip__text_error"}`}>{text}</p>
+    <div className={classNamesContainer}>
+      <p className={classNamesText}>{text}</p>
     </div>
   );
 };
+
 export default InfoToolTip;

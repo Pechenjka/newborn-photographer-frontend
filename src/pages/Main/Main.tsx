472cc1933516296ef7cbb19b7805cf4df9d64ddf
Petr Lobachev <lobachevpetr@gmail.com>
2022-10-26 18:19:48 +0300

    refactor: refactor popular packets

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 import "./Main.scss";
 import PhotoGalleryOfTheMainPage from "./components/PhotoGalleryOfTheMainPage/PhotoGalleryOfTheMainPage";
-import PopularPackets from "./components/PopularPackets/PopularPackets";
+import PopularPackets from "../../components/PopularPackets/PopularPackets";
 import { SliderComponent } from "../../components/SliderComponent";
 
 export const Main: React.FC = () => {

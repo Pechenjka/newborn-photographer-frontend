7c3c3725576d10907b99af067da9d0f280940d04
Petr Lobachev <lobachevpetr@gmail.com>
2022-11-06 16:03:54 +0300

    fix: bugfix

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,35 @@
-import React from "react";
+import React, { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./components/App/App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-// import { createRoot } from "react-dom/client";
+import * as ReactDOMClient from "react-dom/client";
+import { createRoot } from "react-dom/client";
+import { hydrateRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 
-const rootElement = document.getElementById("root") as HTMLElement;
-// const root = createRoot(rootElement);
-
-ReactDOM.hydrate(
-  <BrowserRouter>
-    <Provider store={store}>
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
-    </Provider>
-  </BrowserRouter>,
-  rootElement
-);
-// root.render(
-//   <BrowserRouter>
-//     <Provider store={store}>
+const container = document.getElementById("root") as HTMLElement;
+const root = createRoot(container)
+// ReactDOMClient.hydrateRoot(
+//   rootElement,
+//     <BrowserRouter>
 //       <HelmetProvider>
+//       <Provider store={store}>
 //         <App />
+//       </Provider>
 //       </HelmetProvider>
-//     </Provider>
-//   </BrowserRouter>
+//     </BrowserRouter>
 // );
+
+root.render(
+  <BrowserRouter>
+    <HelmetProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </HelmetProvider>
+  </BrowserRouter>
+);
+

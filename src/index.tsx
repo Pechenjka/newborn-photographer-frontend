77fd131d790b7b9d0a51cd328e754c7052524452
Petr Lobachev <lobachevpetr@gmail.com>
2022-11-11 20:34:08 +0300

    fix: bugfix

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,14 @@
-import React, { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import React from "react";
 import "./index.scss";
 import App from "./components/App/App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import * as ReactDOMClient from "react-dom/client";
 import { createRoot } from "react-dom/client";
-import { hydrateRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 
-const container = document.getElementById("root") as HTMLElement;
-const root = createRoot(container)
-// ReactDOMClient.hydrateRoot(
-//   rootElement,
-//     <BrowserRouter>
-//       <HelmetProvider>
-//       <Provider store={store}>
-//         <App />
-//       </Provider>
-//       </HelmetProvider>
-//     </BrowserRouter>
-// );
+ const container = document.getElementById("root") as HTMLElement;
+ const root = createRoot(container)
 
 root.render(
   <BrowserRouter>
@@ -32,4 +19,3 @@ root.render(
     </HelmetProvider>
   </BrowserRouter>
 );
-

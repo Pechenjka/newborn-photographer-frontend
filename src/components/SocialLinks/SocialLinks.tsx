43f65757de6e2caf7eaeda5434c0ce17612120ac
Petr Lobachev <lobachevpetr@gmail.com>
2023-09-18 23:02:41 -0400

    bug/upd socialLinks

--- src/components/SocialLinks/SocialLinks.tsx
+++ src/components/SocialLinks/SocialLinks.tsx
@@ -1,29 +1,64 @@
-import './SocialLinks.scss'
+import "./SocialLinks.scss";
 import React from "react";
+import TelegramIcon from "../../images/telegram-icon.svg";
+import InstagramIcon from "../../images/instagram-icon.svg";
+import WhatsAppIcon from "../../images/whatsApp-icon.svg";
+import FacebookIcon from "../../images/facebook-icon.svg";
+import YoutubeIcon from "../../images/youtube-icon.svg";
 
-
-const SocialLinks:React.FC = () => {
-  return(
+const SocialLinks: React.FC = () => {
+  return (
     <div className="socialLinks">
       <div className="socialLinks__container">
         <a
           className="socialLinks__item-social-networks socialLinks__item-social-networks_whatsApp"
           href="https://wa.me/15164684837"
-          target='_blank'
-        >{null}</a>
+          target="_blank"
+          rel="noopener noreferrer"
+          title="whatsApp"
+        >
+          <img className="socialLinks__item-social-networksImg" src={WhatsAppIcon} alt="whatsApp" />
+        </a>
         <a
           className="socialLinks__item-social-networks socialLinks__item-social-networks_telegram"
-          href="tg://resolve?domain=newbornphoto_lobacheva"
-          target='_blank'
-        >{null}</a>
+          //href="https://t.me/@alenchik_berry"
+          href="tg://resolve?domain=@alenchik_berry"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="telegram"
+        >
+          <img className="socialLinks__item-social-networksImg" src={TelegramIcon} alt="telegram" />
+        </a>
         <a
           className="socialLinks__item-social-networks socialLinks__item-social-networks_instagram"
-          href="https://www.instagram.com/newbornphoto_lobacheva/"
-          target='_blank'
-        >{null}</a>
+          href="https://www.instagram.com/lobachevaphotography/"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="instagram"
+        >
+          <img className="socialLinks__item-social-networksImg" src={InstagramIcon} alt="instagram" />
+        </a>
+        <a
+          className="socialLinks__item-social-networks socialLinks__item-social-networks_facebook"
+          href="https://www.facebook.com/Alen4ikLobacheva?mibextid=9R9pXO"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="facebook"
+        >
+          <img className="socialLinks__item-social-networksImg" src={FacebookIcon} alt="facebook" />
+        </a>
+        <a
+          className="socialLinks__item-social-networks socialLinks__item-social-networks_youtube"
+          href="https://www.youtube.com/channel/UCUn6A0QHTBpfN-_VAuOQZ5g"
+          target="_blank"
+          rel="noopener noreferrer"
+          title="youtube"
+        >
+          <img className="socialLinks__item-social-networksImg" src={YoutubeIcon} alt="youtube" />
+        </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default SocialLinks;

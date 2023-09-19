723a9c2e700da7e24f7039c55d504c086756e24a
Petr Lobachev <lobachevpetr@gmail.com>
2023-09-18 23:01:56 -0400

    bug/upd multiply language

--- src/18n.js
+++ src/18n.js
@@ -1,16 +1,16 @@
 import i18next from "i18next";
 import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
 
 i18next
   .use(Backend)
-  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    lng: "en",
     fallbackLng: "en",
     debug: true,
     interpolation: {
       escapeValue: false,
     },
-  });
+  })
+  .then((res) => res);

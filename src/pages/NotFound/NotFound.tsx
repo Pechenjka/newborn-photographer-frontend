ff26c1a1d67b309e6946d73d5e5c65bd05fa3d94
Petr Lobachev <lobachevpetr@gmail.com>
2024-04-04 18:11:06 -0400

    improve/upd page not found

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./NotFound.scss";
 import { Link } from "react-router-dom";
-import BackgroundImage from "../../components/BackgroundImage/BackgroundImage";
 import { Helmet } from "react-helmet-async";
 
 const NotFound: React.FC = () => {
@@ -11,7 +10,6 @@ const NotFound: React.FC = () => {
         <meta name="prerender-status-code" content="404" />
         <meta name="robots" content="noindex, nofollow" />
       </Helmet>
-      <BackgroundImage />
       <h2 className="notFound__title">404</h2>
       <p className="notFound__description">Page not found</p>
       <Link to="/" className="notFound__link">

7f520c32f19059d6c9dc069fe4afed85070466bb
Petr Lobachev <lobachevpetr@gmail.com>
2024-04-04 18:31:05 -0400

    bug/fix Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import classNames from "classnames/bind";
 import Styles from "./style.module.scss";
+import { motion } from "framer-motion";
 
 type TypeButton = "button" | "submit";
-export type TypeStyleButton =  "ping" | 'brown' | 'transparent' | 'simply';
+export type TypeStyleButton = "ping" | "brown" | "transparent" | "simply";
 
 interface ButtonProps {
   children: React.ReactNode;
@@ -18,39 +19,53 @@ interface ButtonProps {
   edit?: boolean;
   type: TypeButton;
   form?: string;
-  hide?: boolean
+  hide?: boolean;
 }
 
-export const Button: React.FC<ButtonProps> = ({
-  children,
-  buttonWithIcon = false,
-  icon,
-  styleButton,
-  disabled = false,
-  active = false,
-  styleButtonActive,
-  onClick,
-  editStyle,
-  edit = false,
-  type,
-  form,
-  hide
-}) => {
-  const cx = classNames.bind(Styles);
-  const className = cx(
-    "button",
-    `button__${styleButton}`,
-    { 'button_hide': hide },
-    { [`button__${styleButtonActive}`]: active },
-     { [`button__${styleButton}_disabled`]: disabled },
-     { [`button_disabled`]: disabled },
-    { [`button__${styleButton}_${editStyle}`]: edit }
-  );
-  return (
-    <button  className={className} onClick={onClick} type={type} form={form} disabled={disabled}>
-      {buttonWithIcon && <img src={icon} alt='logo' className={Styles.button__icon} />}
-      {children}
-    </button>
-  );
-};
+export const Button: React.FC<ButtonProps> = forwardRef(
+  (
+    {
+      children,
+      buttonWithIcon = false,
+      icon,
+      styleButton,
+      disabled = false,
+      active = false,
+      styleButtonActive,
+      onClick,
+      editStyle,
+      edit = false,
+      type,
+      form,
+      hide,
+    },
+    ref: any
+  ) => {
+    const cx = classNames.bind(Styles);
+    const className = cx(
+      "button",
+      `button__${styleButton}`,
+      { button_hide: hide },
+      { [`button__${styleButtonActive}`]: active },
+      { [`button__${styleButton}_disabled`]: disabled },
+      { [`button_disabled`]: disabled },
+      { [`button__${styleButton}_${editStyle}`]: edit }
+    );
+    return (
+      <button
+        className={className}
+        onClick={onClick}
+        type={type}
+        form={form}
+        disabled={disabled}
+        ref={ref}
+        title={`${children}`}
+      >
+        {buttonWithIcon && <img src={icon} alt="logo button" className={Styles.button__icon} />}
+        {children}
+      </button>
+    );
+  }
+);
 
+export const MButton = motion(Button);
